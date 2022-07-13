e0424c4e5ccd137b2dba00242c4d9c889b896a7b
Spandan <62278029+maxchalk@users.noreply.github.com>
2022-07-13 21:23:26 +0530

    routing changes and updation

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { RouterModule, Routes } from '@angular/router';
 import { CartPageComponent } from './cart-page/cart-page.component';
 import { FoodpageComponent } from './foodpage/foodpage.component';
 import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
-  {path:'',component:HomeComponent},
+  {path:'',component:HomeComponent,pathMatch:'full'},
   {path:'search/:searchItem',component:HomeComponent},
   {path:'tag/:tag',component:HomeComponent},
   {path:'food/:id',component:FoodpageComponent},
-  {path:'cart-page',component:CartPageComponent}
+  {path:'cart-page',component:CartPageComponent},
+  {path:'login',component:LoginComponent},
+  {path:'signup',component:SignupComponent}
 ];
 
 @NgModule({
