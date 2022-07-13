be869defb6de2586fea6fc5790a156b1f466e141
Spandan <62278029+maxchalk@users.noreply.github.com>
2022-07-13 21:34:43 +0530

    changes in cart file

--- frontend/src/app/shared/models/Cart.ts
+++ frontend/src/app/shared/models/Cart.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { CartItem } from "./cartItem";
 
 export class Cart{
@@ -8,19 +7,6 @@ export class Cart{
         let totalPrice = 0;
         this.items.forEach(item => {
             totalPrice += item.price;
-=======
-import { CartItem } from "./CartItem";
-
-export class Cart{
-    items: CartItem[] = [];
-
-    get totalPrice():number{
-        let totalPrice = 0;
-        this.items.forEach(item =>{
-
-            totalPrice += item.price;
-            
->>>>>>> 69ddf2f63048a90cee5182dc255a816b8266b091
         });
         return totalPrice;
     }
