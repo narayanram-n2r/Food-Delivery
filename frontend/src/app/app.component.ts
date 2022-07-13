9757d787c85d797edea01edac16e7c6c6cf7ccb5
Spandan <62278029+maxchalk@users.noreply.github.com>
2022-07-13 19:41:06 +0530

    changes in app component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { FormsModule } from '@angular/forms';
+import { StarRatingComponent, StarRatingModule } from 'angular-star-rating';
+import { SearchComponent } from './search/search.component';
+import { TagsComponent } from './tags/tags.component';
+import { CartPageComponent } from './cart-page/cart-page.component';
+import { FoodpageComponent } from './foodpage/foodpage.component';
+
+// import { StarRatingComponent } from 'ng-starrating';
+// import { RatingModule } from 'ng-starrating';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    SearchComponent,
+    TagsComponent,
+    CartPageComponent,
+    FoodpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    StarRatingModule
+    // StarRatingComponent,
+    // RatingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  title = 'foodorder';
-}
-
+export class AppModule { }
