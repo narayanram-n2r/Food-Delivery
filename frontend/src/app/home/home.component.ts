f6dd07f86170ad08f903892973cca6f01a7d1225
Spandan <62278029+maxchalk@users.noreply.github.com>
2022-07-13 19:05:43 +0530

    home components changes

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -1,18 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { FoodService } from '../services/food/food.service';
 import { Foods } from '../shared/models/food';
 import { ActivatedRoute } from '@angular/router';
-=======
-<<<<<<< HEAD
-import { ActivatedRoute } from '@angular/router';
-import { FoodService } from '../services/food/food.service';
- 
- 
-=======
-import { ServicesComponent } from '../services/services.component';
->>>>>>> 09fbc5a97eb9a6c91109df7a6b16cedde1e3eee9
->>>>>>> 69ddf2f63048a90cee5182dc255a816b8266b091
 
 @Component({
   selector: 'app-home',
@@ -20,19 +9,12 @@ import { ServicesComponent } from '../services/services.component';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-<<<<<<< HEAD
   foods:Foods[]=[];
   constructor(private fs:FoodService, private route:ActivatedRoute) { }
-=======
-<<<<<<< HEAD
-  foods:string[] = [];
-  constructor(private fs:FoodService, private route: ActivatedRoute) { }
->>>>>>> 69ddf2f63048a90cee5182dc255a816b8266b091
 
   ngOnInit(): void {
     this.route.params.subscribe(params =>{
       if(params['searchItem'])
-<<<<<<< HEAD
       this.foods = this.fs.getAll().filter(food =>food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
       else if(params['tag'])
       this.foods =this.fs.getAllFoodByTag(params['tag'])
@@ -40,25 +22,6 @@ export class HomeComponent implements OnInit {
       this.foods = this.fs.getAll();
     })
 
-=======
-      this.foods = this.fs.getAll().filter(food => food.name.toLowerCase().includes(params['searchItem'].toLowerCase()));
-
-      else if(params['tag'])
-      this.foods = this.fs.getAllFoodByTag(params['tag'])
-
-      else
-      this.foods = this.fs.getAll(); //done
-
-    })
-      
-=======
-foods:string[]= [];
-  constructor(private fs:ServicesComponent) { }
-
-  ngOnInit(): void { 
-    this.foods = this.fs.getAll(); //done
->>>>>>> 09fbc5a97eb9a6c91109df7a6b16cedde1e3eee9
->>>>>>> 69ddf2f63048a90cee5182dc255a816b8266b091
   }
 
 }
