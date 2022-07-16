1552fe6e1fccdbcc5856cc71290d4b329d6c885f
Spandan <62278029+maxchalk@users.noreply.github.com>
2022-07-16 21:34:41 +0530

    updating signup page

--- frontend/src/app/signup/signup.component.ts
+++ frontend/src/app/signup/signup.component.ts
@@ -7,18 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-<<<<<<< HEAD
-=======
- 
->>>>>>> 611d9f6e00ae2e36a25f561bf58089995754b270
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
-<<<<<<< HEAD
-=======
-  
 
->>>>>>> 611d9f6e00ae2e36a25f561bf58089995754b270
+ 
 }
